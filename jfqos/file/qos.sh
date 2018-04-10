af44e1485972be832e8b3707d59f4e4a496b2f94
zhoucong <2630682353@qq.com>
2018-04-10 19:57:47 +0800

    add qos

---
+++ jfqos/file/qos.sh
@@ -0,0 +1,84 @@
+#!/bin/sh /etc/rc.common
+qos_init(){
+	iptables -t mangle -N QOSDOWN
+	iptables -t mangle -N QOSUP
+	iptables -t mangle -I FORWARD -i eth0.2 -j QOSDOWN
+	iptables -t mangle -I FORWARD -o eth0.2 -j QOSUP
+
+#	ifconfig ifb0 up
+	tc qdisc add dev eth0 root handle 1: htb default 999
+#	tc qdisc add dev ifb0 root handle 1: htb default 999
+
+#	tc class add dev ifb0 parent 1: classid 1:1 htb rate 1000kbps prio 1
+	
+	#tc class add dev ifb0 parent 1: classid 1:23 htb rate 150kbps ceil 150kbps prio 7
+
+	#iptables -t mangle -A QOSDOWN -d 192.168.20.120 -j MARK --set-mark 6
+	#iptables -t mangle -A QOSDOWN -d 192.168.20.120 -j RETURN
+	#tc filter add dev ifb0 parent 1: protocol ip prio 1 handle 7 fw classid 1:23
+
+#	tc filter add dev eth0 parent 1: protocol ip prio 1 u32 match u32 0 0 action mirred egress redirect dev ifb0
+}
+
+stop(){
+	iptables -t mangle -F QOSDOWN 
+	iptables -t mangle -F QOSUP
+	iptables -t mangle -D FORWARD -i eth0.2 -j QOSDOWN
+	iptables -t mangle -D FORWARD -o eth0.2 -j QOSUP
+	iptables -t mangle -X QOSDOWN
+	iptables -t mangle -X QOSUP
+	tc qdisc del dev eth0 root
+#	tc qdisc del dev ifb0 root
+}
+
+tcclass_num=1
+
+setup_eachip_qos() 
+{
+	local qos_ip
+	local down_speed
+	local up_speed
+	config_get qos_ip "$1" ipaddr
+	config_get down_speed "$1" download
+	config_get up_speed "$1" upload
+
+	local str_split="-"
+	local range_result=$(echo $qos_ip | grep $str_split)
+
+
+
+	if [ $down_speed ]; then
+		
+		if [ $range_result ]; then
+			iptables -t mangle -A QOSDOWN -m iprange --dst-range ${qos_ip} -j MARK --set-mark ${tcclass_num}
+			iptables -t mangle -A QOSDOWN -m iprange --dst-range ${qos_ip} -j RETURN
+		else
+			iptables -t mangle -A QOSDOWN -d ${qos_ip} -j MARK --set-mark ${tcclass_num}
+			iptables -t mangle -A QOSDOWN -d ${qos_ip} -j RETURN
+		fi
+		tc class add dev eth0 parent 1: classid 1:2${tcclass_num} htb rate ${down_speed}kbps
+		tc filter add dev eth0 parent 1: protocol ip prio 1 handle ${tcclass_num} fw classid 1:2${tcclass_num}
+		tcclass_num=$(($tcclass_num + 1))
+	fi
+
+	if [ $up_speed ]; then
+		if [ $range_result ]; then
+			iptables -t mangle -A QOSUP -m iprange --src-range ${qos_ip} -j MARK --set-mark ${tcclass_num}
+			iptables -t mangle -A QOSUP -m iprange --src-range ${qos_ip} -j RETURN
+		else
+			iptables -t mangle -A QOSUP -d ${qos_ip} -j MARK --set-mark ${tcclass_num}
+			iptables -t mangle -A QOSUP -d ${qos_ip} -j RETURN
+		fi
+		tc class add dev eth0 parent 1: classid 1:2${tcclass_num} htb rate ${up_speed}kbps
+		tc filter add dev eth0 parent 1: protocol ip prio 1 handle ${tcclass_num} fw classid 1:2${tcclass_num}
+		tcclass_num=$(($tcclass_num + 1))
+	fi
+}
+
+
+start()
+{
+	qos_init
+	config_load bandwidth
+	config_foreach setup_eachip_qos client_ip
+}
